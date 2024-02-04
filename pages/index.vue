487ed68e99a18531fea9534aa05b2eee56eed476
AntixxTechHub <info@antixxtechhub.com>
2024-02-04 15:29:08 +0530

    emerging technologies fram

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,14 @@
     <HomeHero />
     <TailoredServices />
     <Statistics />
-    <FunFact />
     <OurProducts />
     <DeliveryChannels />
     <OurMediaAssets />
     <DataStrategy />
     <CertificationsAwards />
+    <EmergingTechnologies />
+    <FunFact />
+    <WhyChooseUs />
     <LetsWorkTogether />
     <LocateUs />
     <HomeCTA />
@@ -20,12 +22,14 @@ import LayoutOne from '~/layouts/LayoutOne.vue';
 import HomeHero from '~/components/index/HomeHero.vue'
 import TailoredServices from '~/components/index/TailoredServices.vue';
 import Statistics from '~/components/index/Statistics.vue';
-import FunFact from '~~/components/index/FunFact.vue';
 import OurProducts from "~/components/index/OurProducts.vue";
 import DeliveryChannels from "~/components/index/DeliveryChannels.vue";
 import OurMediaAssets from '~/components/index/OurMediaAssets.vue';
 import DataStrategy from "~/components/index/DataStrategy";
 import CertificationsAwards from '~/components/index/CertificationsAwards.vue';
+import EmergingTechnologies from '~~/components/index/EmergingTechnologies.vue';
+import FunFact from '~~/components/index/FunFact.vue';
+import WhyChooseUs from '~/components/index/WhyChooseUs.vue';
 import LetsWorkTogether from '~/components/index/LetsWorkTogether.vue';
 import LocateUs from "~/components/index/LocateUs.vue";
 import HomeCTA from "~/components/index/HomeCTA.vue";
