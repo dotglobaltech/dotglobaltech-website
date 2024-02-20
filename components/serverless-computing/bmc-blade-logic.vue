e8a42534ce31922159510aab1d2e9c3becaccf15
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-20 19:23:20 +0530

    aws section done

--- components/serverless-computing/bmc-blade-logic.vue
+++ components/serverless-computing/bmc-blade-logic.vue
@@ -17,11 +17,11 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import ServiceBreadcrumb from '~~/components/breadcrumb/ServiceBreadcrumb.vue';
-import HomeServices from '~~/components/aws-market-enablement/HomeServices.vue';
-import BMCServicesAbout from '~~/components/aws-market-enablement/BMCServicesAbout.vue';
-import ServicesVision from '~~/components/aws-market-enablement/ServicesVision.vue';
-import HomePortfolio from '~~/components/aws-market-enablement/HomePortfolio.vue';
-import ServicesSupportArea from '~~/components/aws-market-enablement/ServicesSupportArea.vue';
+import HomeServices from '~~/components/bmc-blade-logic/HomeServices.vue';
+import BMCServicesAbout from '~~/components/bmc-blade-logic/BMCServicesAbout.vue';
+import ServicesVision from '~~/components/vision/ServicesVision.vue';
+import HomePortfolio from '~~/components/portfolios/HomePortfolio.vue';
+import ServicesSupportArea from '~~/components/supports/ServicesSupportArea.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
--- pages/aws-cloud-migration.vue
+++ pages/aws-cloud-migration.vue
@@ -17,11 +17,11 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import ServiceBreadcrumb from '~~/components/breadcrumb/ServiceBreadcrumb.vue';
-import HomeServices from '~~/components/bmc-helix-itsm/HomeServices.vue';
-import BMCServicesAbout from '~~/components/bmc-helix-itsm/BMCServicesAbout.vue';
-import ServicesVision from '~~/components/vision/ServicesVision.vue';
-import HomePortfolio from '~~/components/portfolios/HomePortfolio.vue';
-import ServicesSupportArea from '~~/components/supports/ServicesSupportArea.vue';
+import HomeServices from '~~/components/aws-cloud-migration/HomeServices.vue';
+import BMCServicesAbout from '~~/components/aws-cloud-migration/BMCServicesAbout.vue';
+import ServicesVision from '~~/components/aws-cloud-migration/ServicesVision.vue';
+import HomePortfolio from '~~/components/aws-cloud-migration/HomePortfolio.vue';
+import ServicesSupportArea from '~~/components/aws-cloud-migration/ServicesSupportArea.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
