15cdc3aae94a991e6e049a2a57260d181348bb51
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-20 18:42:52 +0530

    servicenow section done

--- components/servicenow-it-workflows/bmc-blade-logic.vue
+++ components/servicenow-it-workflows/bmc-blade-logic.vue
@@ -17,11 +17,11 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import ServiceBreadcrumb from '~~/components/breadcrumb/ServiceBreadcrumb.vue';
-import HomeServices from '~~/components/servicenow-configuration-compliance/HomeServices.vue';
-import BMCServicesAbout from '~~/components/servicenow-configuration-compliance/BMCServicesAbout.vue';
-import ServicesVision from '~~/components/servicenow-configuration-compliance/ServicesVision.vue';
-import HomePortfolio from '~~/components/servicenow-configuration-compliance/HomePortfolio.vue';
-import ServicesSupportArea from '~~/components/servicenow-configuration-compliance/ServicesSupportArea.vue';
+import HomeServices from '~~/components/bmc-blade-logic/HomeServices.vue';
+import BMCServicesAbout from '~~/components/bmc-blade-logic/BMCServicesAbout.vue';
+import ServicesVision from '~~/components/vision/ServicesVision.vue';
+import HomePortfolio from '~~/components/portfolios/HomePortfolio.vue';
+import ServicesSupportArea from '~~/components/supports/ServicesSupportArea.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
--- pages/servicenow-it-operations-management.vue
+++ pages/servicenow-it-operations-management.vue
@@ -17,11 +17,11 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import ServiceBreadcrumb from '~~/components/breadcrumb/ServiceBreadcrumb.vue';
-import HomeServices from '~~/components/bmc-helix-itsm/HomeServices.vue';
-import BMCServicesAbout from '~~/components/bmc-helix-itsm/BMCServicesAbout.vue';
-import ServicesVision from '~~/components/vision/ServicesVision.vue';
-import HomePortfolio from '~~/components/portfolios/HomePortfolio.vue';
-import ServicesSupportArea from '~~/components/supports/ServicesSupportArea.vue';
+import HomeServices from '~~/components/servicenow-it-operations-management/HomeServices.vue';
+import BMCServicesAbout from '~~/components/servicenow-it-operations-management/BMCServicesAbout.vue';
+import ServicesVision from '~~/components/servicenow-it-operations-management/ServicesVision.vue';
+import HomePortfolio from '~~/components/servicenow-it-operations-management/HomePortfolio.vue';
+import ServicesSupportArea from '~~/components/servicenow-it-operations-management/ServicesSupportArea.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
