77f27fb2d2f170e6b5eb67d5721ed61293f4a3b5
AntixxTechHub <info@antixxtechhub.com>
2024-02-20 17:46:56 +0530

    Blade logic

--- components/bmc-blade-logic/bmc-blade-logic.vue
+++ components/bmc-blade-logic/bmc-blade-logic.vue
@@ -16,13 +16,13 @@
 
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
-import ServiceBreadcrumb from '~~/components/bmc-blade-logic/ServiceBreadcrumb.vue';
+import ServiceBreadcrumb from '~~/components/breadcrumb/ServiceBreadcrumb.vue';
 import HomeServices from '~~/components/bmc-blade-logic/HomeServices.vue';
 import BMCServicesAbout from '~~/components/bmc-blade-logic/BMCServicesAbout.vue';
-import ServicesVision from '~~/components/bmc-blade-logic/ServicesVision.vue';
-import HomePortfolio from '~~/components/bmc-blade-logic/HomePortfolio.vue';
-import ServicesSupportArea from '~~/components/bmc-blade-logic/ServicesSupportArea.vue';
-import HomeCTA from "~/components/subscribe/SubscribeNow.vue";
+import ServicesVision from '~~/components/vision/ServicesVision.vue';
+import HomePortfolio from '~~/components/portfolios/HomePortfolio.vue';
+import ServicesSupportArea from '~~/components/supports/ServicesSupportArea.vue';
+import HomeCTA from "~/components/index/HomeCTA.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
 import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
