2926c931f15ad1b756245c80e2580e2df9cc7fc5
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-20 18:01:28 +0530

    footer added

--- pages/portfolio-2.vue
+++ pages/portfolio-2.vue
@@ -5,7 +5,8 @@
     <portfolio-breadcrumb/>
     <grid-three-columns/>
     <start-up-cta :style_2="true"/>
-    <footer-six :style_2="true" />
+    <FooterFour />
+    <footer-eight/>
     <back-to-top />
   </div>
 </template>
@@ -15,11 +16,12 @@ import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import PortfolioBreadcrumb from '~~/components/breadcrumb/PortfolioBreadcrumb.vue';
 import StartUpCta from '~~/components/cta/StartUpCta.vue';
 import GridThreeColumns from '~~/components/portfolios/portfolio-page/GridThreeColumns.vue';
-import FooterSix from '~~/layouts/footers/FooterSix.vue';
+import FooterFour from '~~/layouts/footers/FooterFour.vue';
+import FooterEight from '~~/layouts/footers/FooterEight.vue';
 import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
 
 export default {
-  components: { HeaderOne, PortfolioBreadcrumb, GridThreeColumns, StartUpCta, FooterSix, BackToTop},
+  components: { HeaderOne, PortfolioBreadcrumb, GridThreeColumns, StartUpCta, FooterFour, FooterEight, BackToTop},
   setup() {
     useHead({
       title: "Portfolio 2 - Creative Agency & Portfolio Vue Nuxt 3 Template",
