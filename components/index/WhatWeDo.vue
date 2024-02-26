95e8c61e6bbdb4e5db6e9a4c4ad38621e2b6dd30
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-26 18:32:26 +0530

    custom icon added

--- components/index/WhatWeDo.vue
+++ components/index/WhatWeDo.vue
@@ -53,7 +53,7 @@
                           <div class="img-iconbb"><img src="~/assets/img/icon/software-consulting.png" alt="img"></div>
                         </nuxt-link></li>
                       <li><nuxt-link href="/">
-                          <div class="img-iconbb"><img src="~/assets/img/icon/php.svg" alt="img"></div>
+                          <div class="img-iconbb"><img src="~/assets/img/icon/custom-software-developmen.png" alt="img"></div>
                         </nuxt-link></li>
                       <li><nuxt-link href="/">
                           <div class="img-iconbb"><img src="~/assets/img/icon/java.svg" alt="img"></div>
