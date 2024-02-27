4607e580d57c098782792fa45341183832691516
AntixxTechHub <info@antixxtechhub.com>
2024-02-27 17:18:11 +0530

    Funfact

--- components/index/PoliticianCounter.vue
+++ components/index/PoliticianCounter.vue
@@ -199,7 +199,7 @@ export default {
           delay: ".3s",
           num: 98,
           text: "%",
-          title: "Project Delivered",
+          title: "Projectlijl Delivered",
         },
         {
           icon: `<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
