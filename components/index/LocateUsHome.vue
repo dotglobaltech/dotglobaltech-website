bb259be2d95964fad071ab0f24937e2a8514194d
AntixxTechHub <info@antixxtechhub.com>
2024-02-28 13:48:45 +0530

    New changes

--- components/index/LocateUsHome.vue
+++ components/index/LocateUsHome.vue
@@ -1,98 +1,81 @@
 <template>
-    <section class="features__area pt-80 pb-20">
-      <div class="container">
-        <div class="row justify-content-center">
-                <div class="col-xxl-7 col-xl-8 col-lg-9">
-                    <div class="section__title-wrapper-7 text-center mb-60">
-                        <span class="section__title-pre-2">Locate Us</span>
-                        <h3 class="section__title-7">
-                            <span class="section__title-7-highlight">
-                                Locate Us
-                                <svg width="240" height="22" viewBox="0 0 240 22" fill="none">
-                                    <path class="wow" fill-rule="evenodd" clip-rule="evenodd"
-                                        d="M0.440833 21.1152C78.9497 6.16225 158.05 4.53412 235.949 13.8239C237.497 14.0088 239.796 12.4065 239.988 9.93474C240.181 7.4176 238.026 5.44088 236.474 5.2332C157.99 -5.31675 79.1936 0.359501 0.316568 19.7785C-0.184784 19.9023 -0.0511379 21.2092 0.440833 21.1152Z"
-                                        fill="currentColor" />
-                                </svg>
-                            </span>
-                        </h3>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-        <div class="row justify-content-center">
-          <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
-            <div class="features__item-13 d-flex align-items-start mb-40">
-              <div class="features__icon-13">
-                <span>
-                    <img src="~/assets/img/home/india.svg" alt="Top-Notch Global Brands" />
-                </span>
-              </div>
-              <div class="features__content-13">
-                <h3 class="features__title-13">INDIA (HQ)</h3>
-                <p>C-106/B, Ganesh Meridian, Opp. Gujarat High Court, S. G. Highway, Ahmedabad, Gujarat, 380061.</p>
-                <p><strong>+91 96060 28218</strong></p>
-              </div>
-            </div>
+  <section class="features__area pt-80 pb-20">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-7 col-xl-8 col-lg-9">
+          <div class="section__title-wrapper-7 text-center mb-60">
+            <span class="section__title-pre-2">Locate Us</span>
+            <h3 class="section__title-7">
+              <span class="section__title-7-highlight">
+                Locate Us
+                <svg width="240" height="22" viewBox="0 0 240 22" fill="none">
+                  <path class="wow" fill-rule="evenodd" clip-rule="evenodd"
+                    d="M0.440833 21.1152C78.9497 6.16225 158.05 4.53412 235.949 13.8239C237.497 14.0088 239.796 12.4065 239.988 9.93474C240.181 7.4176 238.026 5.44088 236.474 5.2332C157.99 -5.31675 79.1936 0.359501 0.316568 19.7785C-0.184784 19.9023 -0.0511379 21.2092 0.440833 21.1152Z"
+                    fill="currentColor" />
+                </svg>
+              </span>
+            </h3>
           </div>
-
-          <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
-            <div class="features__item-13 d-flex align-items-start mb-40">
-              <div class="features__icon-13">
-                <span>
-                    <img src="~/assets/img/home/uae.svg" alt="Top-Notch Global Brands" />
-                </span>
-              </div>
-              <div class="features__content-13">
-                <h3 class="features__title-13">UAE</h3>
-                <p>Industrial Area, Sharja.</p>
-                <p><strong>+971 05048 26491</strong></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
-            <div class="features__item-13 d-flex align-items-start mb-40">
-              <div class="features__icon-13">
-                <span>
-                    <img src="~/assets/img/home/saudi-arabia.svg" alt="Top-Notch Global Brands" />
-                </span>
-              </div>
-              <div class="features__content-13">
-                <h3 class="features__title-13">Saudi Arabia</h3>
-                <p></p>
-                <p><strong>+966 505522815</strong></p>
-              </div>
-            </div>
-          </div>
-
-          <!-- <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
-            <div class="features__item-13 d-flex align-items-start mb-40">
-              <div class="features__icon-13">
-                <span>
-                    <img src="~/assets/img/home/india.svg" alt="Top-Notch Global Brands" />
-                </span>
-              </div>
-              <div class="features__content-13">
-                <h3 class="features__title-13">UAE</h3>
-                <p>C-106/B, Ganesh Meridian, Opp. Gujarat High Court, S. G. Highway, Ahmedabad, Gujarat, 380061</p>
-                <p><strong>+91 96060 28218</strong></p>
-              </div>
-            </div>
-          </div> -->
         </div>
       </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    data () {
-      return {
 
-      }
+      <div class="row justify-content-center">
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+          <div class="features__item-13 d-flex align-items-start mb-40">
+            <div class="features__icon-13">
+              <span>
+                <img src="~/assets/img/home/location-cons-1.svg" alt="Top-Notch Global Brands" />
+              </span>
+            </div>
+            <div class="features__content-13">
+              <h3 class="features__title-13">INDIA (HQ)</h3>
+              <p>C-106/B, Ganesh Meridian, Opp. Gujarat High Court, S. G. Highway, Ahmedabad, Gujarat, 380061.</p>
+              <p><strong>+91 96060 28218</strong></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+          <div class="features__item-13 d-flex align-items-start mb-40">
+            <div class="features__icon-13">
+              <span>
+                <img src="~/assets/img/home/location-cons-2.svg" alt="Top-Notch Global Brands" />
+              </span>
+            </div>
+            <div class="features__content-13">
+              <h3 class="features__title-13">UAE</h3>
+              <p>Industrial Area, Sharja.</p>
+              <p><strong>+971 05048 26491</strong></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+          <div class="features__item-13 d-flex align-items-start mb-40">
+            <div class="features__icon-13">
+              <span>
+                <img src="~/assets/img/home/location-cons-3.svg" alt="Top-Notch Global Brands" />
+              </span>
+            </div>
+            <div class="features__content-13">
+              <h3 class="features__title-13">Saudi Arabia</h3>
+              <p></p>
+              <p><strong>+966 505522815</strong></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+
     }
   }
-  </script>
+}
+</script>
