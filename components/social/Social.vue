50d1a3ffe697140141ad37f6548ad9f75a3c4c47
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-28 17:04:15 +0530

    links added

--- components/social/Social.vue
+++ components/social/Social.vue
@@ -27,19 +27,19 @@ export default {
           name: "Twitter",
         },
         {
-          link: "https://www.linkedin.com/",
+          link: "https://www.linkedin.com/company/dot-global-technologies",
           target: "_blank",
           icon: "fa-brands fa-linkedin-in",
           name: "Linkedin",
         },
         {
-          link: "https://www.youtube.com/",
+          link: "https://www.youtube.com/channel/UCLGScKccLcq0qDkx5epBemw",
           target: "_blank",
           icon: "fa-brands fa-youtube",
           name: "Youtube",
         },
         {
-          link: "https://www.youtube.com/",
+          link: "https://www.instagram.com/dotglobaltech/",
           target: "_blank",
           icon: "fa-brands fa-instagram",
           name: "Youtube",
