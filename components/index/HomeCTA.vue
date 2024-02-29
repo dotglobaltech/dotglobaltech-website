45ce74b1b769c65ea7a2ba2a897ee728df536f85
AntixxTechHub <info@antixxtechhub.com>
2024-02-29 10:36:41 +0530

    cta update

--- components/index/HomeCTA.vue
+++ components/index/HomeCTA.vue
@@ -6,26 +6,33 @@
             <div class="row align-items-center">
                 <div class="col-xxl-6 col-xl-6 col-lg-8">
                     <h3 class="cta__title-5">Get New Insights Weekly</h3>
-                    <div class="cta__content-5"><span>Stay ahead of the curve with our weekly insights delivered straight to your inbox. Sign up now to receive the latest trends, analysis, and expert perspectives on the IT Industry. Enter your email to subscribe.</span>
+                    <div class="cta__content-5"><span>Stay ahead of the curve with our weekly insights delivered straight to
+                            your inbox. Sign up now to receive the latest trends, analysis, and expert perspectives on the
+                            IT Industry. Enter your email to subscribe.</span>
                     </div>
                 </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-4">
-                    <div class="cta__form-13">
-                        <form action="#">
-                            <div class="cta__input-13">
-                                <input type="email" placeholder="Enter Your Email">
-                                <div class="cta__btn-5 text-lg-end">
-                                    <nuxt-link href="/" class="tp-btn btn-cta bg-btn lnk">
-                                        Subscribe
+
+                <div class="col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-11">
+                    <div class="subscribe__wrapper text-center">
+                        <div class="subscribe__form text-start">
+                            <form>
+                                <div class="subscribe__input">
+                                    <input type="email" placeholder="Enter your email address..." />
+                                    <button class="bg-btn lnk" type="submit">Subscribe
                                         <i class="fas fa-chevron-right fa-icon"></i>
                                         <span class="circle"></span>
-                                    </nuxt-link>
+                                    </button>
                                 </div>
-                                <!-- <button type="submit" class="tp-btn">Subscribe</button> -->
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
+
+
+
+
+
+
             </div>
         </div>
     </section>
