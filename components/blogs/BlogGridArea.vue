c8b25358d26faace938ac3683af712fd9dab6fa3
AntixxTechHub <info@antixxtechhub.com>
2024-02-29 13:02:57 +0530

    Blog grid content

--- components/blogs/BlogGridArea.vue
+++ components/blogs/BlogGridArea.vue
@@ -17,18 +17,68 @@
           </div>
         </div>
       </div>
-      <div class="row">
 
-        <div v-for="(item, i) in blogData.filter((b) => b.blog_grid)" :key="i"
-          class="col-xxl-4 col-xl-4 col-lg-6 col-md-6">
-          <single-blog-grid-item :item="item" />
+      <div class="row justify-content-center">
+        <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6" v-for="blog in blogs.slice(
+          (currentPage - 1) * perPage,
+          currentPage * perPage,
+        )" :key="blog.id">
+          <div class="blog__item-10 white-bg transition-3 mb-30 fix">
+            <div class="blog__thumb-10 w-img fix">
+              <NuxtLink :to="'/blog-details/' + blog.attributes.slug" class="d-block">
+                <img :src="blog.attributes.image.data.attributes.url" alt="blog">
+              </NuxtLink>
+
+              <div class="blog__tag-10">
+                <!-- <a href="#">{{ item.tag }}</a> -->
+              </div>
+            </div>
+            <div class="blog__content-10">
+              <div class="blog__content-10-top">
+                <div class="blog__tag-10">
+                  <!-- <a href="#">{{ item.tag }}</a> -->
+                </div>
+                <h3 class="blog__title-10">
+                  <NuxtLink :to="'/blog-details/' + blog.attributes.slug">
+                    {{ blog.attributes.title }}
+                  </NuxtLink>
+                </h3>
+
+                <p>{{ blog.attributes.shortDesc }}...</p>
+              </div>
+              <div class="blog__content-10-bottom d-flex align-items-center justify-content-between">
+                <div class="blog__meta-author-10 d-flex align-items-center">
+                  <div class="blog__meta-author-thumb-10">
+                    <a href="#">
+                      <img :src="blog.attributes.avtar.data.attributes.url" alt="avtar">
+                    </a>
+                  </div>
+                  <div class="blog__meta-author-content-10">
+                    <span>
+                      By <a href="#">{{ blog.attributes.author }}</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="blog__meta-10 blog-meta-10-2">
+                  <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z"
+                      stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5"
+                      stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  {{ blog.attributes.date }}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
+
       <div class="row">
         <div class="col-xxl-12">
           <div class="tp-pagination mt-20">
-            <blog-pagination/>
+            <blog-pagination />
           </div>
         </div>
       </div>
@@ -37,12 +87,24 @@
 </template>
 
 <script>
-import blogData from '~~/mixins/blogData';
+
 import BlogPagination from '../common/pagination/BlogPagination.vue';
-import SingleBlogGridItem from './single-blog/SingleBlogGridItem.vue';
+import axios from 'axios'
 
 export default {
-  components: { SingleBlogGridItem, BlogPagination },
-  mixins: [blogData]
+  components: { BlogPagination },
+  data() {
+    return {
+      blogs: [],
+      rows: 0,
+      currentPage: 1,
+      perPage: 9,
+    }
+  },
+  created: async function () {
+    const response = await axios.get('https://evolvestrapi.pbwebvision.in/api/blogs?populate=*')
+    this.blogs = response.data.data.sort((b, a) => a.id - b.id);
+    this.rows = this.blogs?.length;
+  },
 }
 </script>
