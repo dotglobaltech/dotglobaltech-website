ca16395d7b1c28d06f922df41d770a0efdbd8762
AntixxTechHub <info@antixxtechhub.com>
2024-03-08 13:45:50 +0530

    Content open

--- components/case-studies/CategoryGridContents.vue
+++ components/case-studies/CategoryGridContents.vue
@@ -2,18 +2,18 @@
   <section class="blog__grid grey-bg-4 pt-90 pb-100">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6" v-for="casestudy in details[0].attributes?.blogs?.data"
+        <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6" v-for="casestudy in details[0].attributes?.case_studies?.data"
           :key="casestudy.id">
           <div class="blog__item-10 white-bg transition-3 mb-30 fix">
             <div class="blog__thumb-10 w-img fix">
-              <NuxtLink :to="'/blog-details/' + casestudy.attributes.slug" class="d-block">
+              <NuxtLink :to="'/case-studies/' + casestudy.attributes.slug" class="d-block">
                 <img :src="casestudy?.attributes?.image?.data?.attributes?.url" alt="blog">
               </NuxtLink>
             </div>
             <div class="blog__content-10">
               <div class="blog__content-10-top">
                 <h3 class="blog__title-10">
-                  <NuxtLink :to="'/blog-details/' + casestudy.attributes.slug">
+                  <NuxtLink :to="'/case-studies/' + casestudy.attributes.slug">
                     {{ casestudy.attributes.title }}
                   </NuxtLink>
                 </h3>
