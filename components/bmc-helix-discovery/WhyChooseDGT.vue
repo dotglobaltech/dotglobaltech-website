a099afd06c2e9a422d1af4370e79f6dc036e62cc
AntixxTechHub <info@antixxtechhub.com>
2024-03-15 12:31:51 +0530

    New component

--- components/bmc-helix-discovery/WhyChooseDGT.vue
+++ components/bmc-helix-discovery/WhyChooseDGT.vue
@@ -1,76 +1,54 @@
 <template>
-  <section class="about__area p-relative z-index-1 pt-120 pb-140">
-    <div class="about__shape">
-      <img
-        class="about__shape-7"
-        src="~/assets/img/about/7/about-shape-1.png"
-        alt="about__shape"
-      />
-    </div>
+  <section class="about__area p-relative z-index-1 pt-50 pb-50">
     <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-8 col-xl-8 col-lg-10 col-md-10">
+          <div class="section__title-wrapper-7 mb-60 text-center">
+            <div class="section__title-wrapper-7 mb-40">
+              <span class="section__title-pre-7">Why Choose Dot Global Technologies</span>
+              <h3 class="section__title-7">
+                As Your BMC Helix Discovery
+                <span class="section__title-7-highlight">
+                  Vendor?
+                  <svg width="240" height="22" viewBox="0 0 240 22" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0.440833 21.1152C78.9497 6.16225 158.05 4.53412 235.949 13.8239C237.497 14.0088 239.796 12.4065 239.988 9.93474C240.181 7.4176 238.026 5.44088 236.474 5.2332C157.99 -5.31675 79.1936 0.359501 0.316568 19.7785C-0.184784 19.9023 -0.0511379 21.2092 0.440833 21.1152Z"
+                      fill="currentColor" />
+                  </svg>
+                </span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="row">
         <div class="col-xxl-6 col-lg-6">
-          <div class="about__thumb-wrapper-7 pr-70">
-            <div class="about__thumb-7">
-              <img src="~/assets/img/about/7/about-img-1.jpg" alt="about__thumb" />
+          <div class="about__thumb-wrapper-14 p-relative" data-sal="slide-up" data-sal-delay="130"
+            data-sal-duration="1000">
+            <div class="about__shape">
+              <img class="about__shape-8" src="~/assets/img/about/14/about-shape-1.png" alt="">
+              <img class="about__shape-9" src="~/assets/img/about/14/about-shape-2.png" alt="">
             </div>
-            <div class="about__analysis">
-              <div class="about__analysis-top">
-                <div class="about__analysis-icon">
-                  <span><i class="fa-solid fa-arrow-trend-up"></i></span>
-                  <p>Data Analysis</p>
-                </div>
-                <div class="about__analysis-content">
-                  <div class="about__analysis-item">
-                    <h4>$48,662.44</h4>
-                    <p>Your monthly saving</p>
-                  </div>
-                  <div class="about__analysis-item-2">
-                    <h4>$2,485,166.42</h4>
-                    <p>Your total saving</p>
-                  </div>
-                  <div class="about__analysis-discount">
-                    <span>-32.56%</span>
-                  </div>
-                </div>
-              </div>
+            <div class="about__thumb-14 m-img">
+              <img class="about-img-1" src="~/assets/img/about/14/about-img-1.jpg" alt="">
+              <img class="about-img-2" src="~/assets/img/about/14/about-img-2.jpg" alt="">
             </div>
           </div>
         </div>
         <div class="col-xxl-6 col-lg-6">
           <div class="about__wrapper-7 pt-35">
-            <div class="section__title-wrapper-7 mb-40">
-              <span class="section__title-pre-7">Who we Are</span>
-              <h3 class="section__title-7">
-                We know your problem &
-                <span class="section__title-7-highlight">
-                  solution
-                  <svg width="240" height="22" viewBox="0 0 240 22" fill="none">
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M0.440833 21.1152C78.9497 6.16225 158.05 4.53412 235.949 13.8239C237.497 14.0088 239.796 12.4065 239.988 9.93474C240.181 7.4176 238.026 5.44088 236.474 5.2332C157.99 -5.31675 79.1936 0.359501 0.316568 19.7785C-0.184784 19.9023 -0.0511379 21.2092 0.440833 21.1152Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </span>
-              </h3>
-
-              <p>
-                Understand the growth of your product and direct <br />
-                it better, with Harry.
-              </p>
-            </div>
             <div class="about__list about__list-counter">
               <div class="about__list-item d-flex align-items-start">
                 <div class="about__list-icon">
                   <span></span>
                 </div>
                 <div class="about__list-content">
-                  <h3 class="about__list-title">See the action in live</h3>
+                  <h3 class="about__list-title">Experience you can trust</h3>
                   <p>
-                    Our consultants specialise in one of five practice
-                    development.
+                    Our team of certified BMC Helix Discovery experts possesses extensive experience in designing,
+                    implementing, and managing the solution, ensuring a smooth and successful journey. The extensive
+                    experience and expertise in implementation and support make us the best BMC Helix Discovery
+                    Third-Party Vendor in india.
                   </p>
                 </div>
               </div>
@@ -79,24 +57,26 @@
                   <span></span>
                 </div>
                 <div class="about__list-content">
-                  <h3 class="about__list-title">See the action in live</h3>
+                  <h3 class="about__list-title">Tailored solutions for your unique needs</h3>
                   <p>
-                    Our consultants specialise in one of five practice
-                    development.
+                    We go beyond the one-size-fits-all approach, customizing our services to your specific requirements
+                    and maximizing your ROI.
                   </p>
                 </div>
               </div>
-            </div>
-            <div class="about__btn-7">
-              <nuxt-link
-                href="/about"
-                class="tp-btnr-border tp-btn-shine-effect tp-link-btn-3"
-              >
-                More About us
-                <span>
-                  <i class="fa-regular fa-arrow-right"></i>
-                </span>
-              </nuxt-link>
+              <div class="about__list-item d-flex align-items-start">
+                <div class="about__list-icon">
+                  <span></span>
+                </div>
+                <div class="about__list-content">
+                  <h3 class="about__list-title">Comprehensive support that lasts</h3>
+                  <p>
+                    We are the most trusted BMC Helix Discovery Support Service provider, offering ongoing support, BMC
+                    Helix Discovery Trainings, and maintenance to ensure the continued success of your BMC Helix
+                    Discovery implementation.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -106,5 +86,7 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Why Choose Us'
+};
 </script>
