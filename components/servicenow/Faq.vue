3d43f9effe7ef71beb4a5e176ee5bd3494dca1e2
AntixxTechHub <info@antixxtechhub.com>
2024-04-08 10:27:43 +0530

    Faq added

--- components/servicenow/Faq.vue
+++ components/servicenow/Faq.vue
@@ -9,7 +9,125 @@
           <div class="col-xxl-9 col-xl-9 col-lg-9">
             <div class="faq__wrapper tp-accordion pt-65">
               <div class="accordion" id="faqaccordion">
-                  <single-faq v-for="(item,i) in faq_items" :key="i" :item="item" />
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                      Q 1: What's ServiceNow?
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> It's the cloud-based maestro of automation, streamlining workflows and
+                        empowering your business with a single platform.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                      Q2: What does ServiceNow implementation cost?
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Dot Global Technologies, your trusted ServiceNow consultant, composes
+                        custom plans based on your needs and project scope, maximizing your ServiceNow ROI. We strive to
+                        offer ServiceNow customization, deployment, and support services at the most reasonable and
+                        competitive prices.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                      Q3: What are the types of integration in ServiceNow?
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Here are the common types of integration in ServiceNow:
+                      </p>
+                      <ul>
+                        <li><strong>Sending out ServiceNow data:</strong> Share your platform's magic with other systems
+                          like a generous chef.</li>
+                        <li><strong>Welcoming external data:</strong> Invite information from outside to dance with
+                          ServiceNow's internal rhythm.</li>
+                        <li><strong>Bridging the on-premise gap:</strong> The MID Server builds a friendly bridge for
+                          old-school systems to join the party.</li>
+
+                        <li><strong>Automating data handshakes:</strong> Let business rules choreograph seamless data
+                          exchanges, keeping everything flowing smoothly.</li>
+                        <li><strong>Bulk data migration:</strong> Bring a whole crowd of data from outside in with one
+                          grand import set.</li>
+                        <li><strong>Coding custom connections:</strong> Write your script to control every step for
+                          intricate data dances.</li>
+                        <li><strong>Plug-and-play integrations:</strong> Skip the setup and waltz right in with
+                          pre-built connectivity for popular apps.</li>
+                        <li><strong>IntegrationHub maestro:</strong> Get a central conductor to manage all your data
+                          partnerships effortlessly.</li>
+                      </ul>
+
+                      <br>
+
+                      <h4>Choosing the appropriate integration method depends on factors such as:</h4>
+                      <ul>
+                        <li>The systems required to integrate</li>
+                        <li>The complexity of the data and processes involved</li>
+                        <li>The level of control and customization required</li>
+                        <li>The security and compliance requirements of your organization</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                      Q4: Why do you need a ServiceNow third-party vendor?
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> You require a <strong>ServiceNow third-party vendor</strong> for several
+                        compelling reasons:
+                      </p>
+                      <ul>
+                        <li><strong>Expertise:</strong> Navigates complex setups & customizations with seasoned
+                          knowledge.</li>
+                        <li><strong>Efficiency:</strong> A ready-made team saves time & resources on hiring & training.
+                        </li>
+                        <li><strong>Scalability:</strong> Flexible support models suit large projects & ongoing
+                          maintenance.</li>
+                        <li><strong>Objectivity:</strong> A fresh perspective identifies improvement opportunities.</li>
+                        <li><strong>Cost-effectiveness:</strong> Optimizes workflows, minimizes mistakes, and boosts
+                          ROI.</li>
+                        <li><strong>Specialized resources:</strong> Offers niche skills like security, governance, and
+                          industry integrations.</li>
+                        <li><strong>Reduced risk:</strong> Proven track record & best practices mitigate implementation
+                          & instability risks.</li>
+                      </ul>
+                      <br>
+                      <p>Ultimately, the decision to work with a <strong>ServiceNow third-party vendor</strong> can be a
+                        game-changer, as they focus on your business-specific needs, budget, and available resources.
+                        Remember, a good <strong>ServiceNow vendor</strong>, like Dot Global Technologies, is a partner
+                        in your ServiceNow success, not just a provider of services.</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -20,16 +138,8 @@
 </template>
 
 <script>
-import commonFaq from '~~/mixins/servicenowFAQ';
-import SingleFaq from './Faq/SingleFaq.vue';
 
 export default {
-  props: {
-    parent_id:String,
-  },
-  components:{
-    SingleFaq,
-  },
-  mixins:[commonFaq]
+
 };
 </script>
