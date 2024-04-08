d82013b7fdb0089192470a5a3d2b9a0171b44c6a
AntixxTechHub <info@antixxtechhub.com>
2024-04-08 13:12:36 +0530

    New changes

--- pages/servicenow.vue
+++ pages/servicenow.vue
@@ -2,14 +2,15 @@
   <div>
     <header-one :top_bar="false" :commonOffcanvas="true" />
     <ServiceBreadcrumb />
-    <about-text-area />
-    <personal-port-awards :style_2="true" />
-    <about-faqs />
-    <home-teams :style_2="true" />
-    <agency-brands :spacing="true" :style_2="true" />
+    <ServicenowAbout />
+    <ServicesKeyFeatures />
+    <ServicesVision />
+    <WhyChooseDGT />
+    <DiscoveryKeyFeatures />
+    <Faq />
     <HomeCTA />
     <FooterFour />
-    <footer-eight/>
+    <footer-eight />
     <back-to-top />
   </div>
 </template>
@@ -17,33 +18,36 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import ServiceBreadcrumb from "~~/components/servicenow/ServiceBreadcrumb.vue";
-import AboutTextArea from "~~/components/abouts/about-page-com/AboutTextArea.vue";
-import PersonalPortAwards from "~~/components/awards/PersonalPortAwards.vue";
-import AboutFaqs from "~~/components/faqs/AboutFaqs.vue";
-import HomeTeams from "~~/components/teams/HomeTeams.vue";
-import AgencyBrands from '~~/components/brands/AgencyBrands.vue';
+import ServicenowAbout from '~~/components/servicenow/ServicenowAbout.vue';
+import ServicesKeyFeatures from '~~/components/servicenow/ServicesKeyFeatures.vue';
+import ServicesVision from '~~/components/servicenow/ServicesVision.vue';
+import WhyChooseDGT from '~~/components/servicenow/WhyChooseDGT.vue';
+import DiscoveryKeyFeatures from '~~/components/servicenow/DiscoveryKeyFeatures.vue';
+import Faq from '~~/components/servicenow/Faq.vue';
 import HomeCTA from "~/components/subscribe/SubscribeNow.vue";
 import FooterFour from '~~/layouts/footers/FooterFour.vue';
 import FooterEight from '~~/layouts/footers/FooterEight.vue';
-import BackToTop from "~~/layouts/footers/component/BackToTop.vue";
+import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
 
 export default {
   components: {
     HeaderOne,
-    BackToTop,
     ServiceBreadcrumb,
-    AboutTextArea,
-    PersonalPortAwards,
-    AboutFaqs,
-    HomeTeams,
-    AgencyBrands,
-    HomeCTA,
+    ServicenowAbout,
+    ServicesKeyFeatures,
+    ServicesVision,
+    WhyChooseDGT,
+    DiscoveryKeyFeatures,
+    Faq,
     FooterFour,
     FooterEight,
+    HomeCTA,
+    BackToTop,
   },
+
   setup() {
     useHead({
-      title: "ServiceNow - Dot Global Technologies",
+      title: "ServiceNow  - Dot Global Technologies",
     });
   },
 };
