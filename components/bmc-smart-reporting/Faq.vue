220b06ea38c241bb2886349e24ae38643c66abeb
dotglobaltech <dotglobaltech@gmail.com>
2024-04-08 20:18:55 +0530

    bmc reporting faq

--- components/bmc-smart-reporting/Faq.vue
+++ components/bmc-smart-reporting/Faq.vue
@@ -9,7 +9,89 @@
           <div class="col-xxl-9 col-xl-9 col-lg-9">
             <div class="faq__wrapper tp-accordion pt-65">
               <div class="accordion" id="faqaccordion">
-                  <single-faq v-for="(item,i) in faq_items" :key="i" :item="item" />
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                      Q1: What is BMC Smart Reporting?
+
+                         <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">     
+                      <p><strong>Ans:</strong> BMC Smart Reporting is a user-friendly tool that lets even non-techies build reports. It uses drag-and-drop features to format reports and pick data.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                      Q2:  What's BMC Smart Reporting used for?
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Analyzing your IT data! It provides easy-to-use features for in-depth analysis and tracking of key business performance metrics.</p>
+                    
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                      Q3: How much does BMC Smart Reporting cost?
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> The BMC Smart Reporting Pricing depends on your specific needs and the project's size. Contact Zivra Technologies for a quote on customization, deployment, support, and training.</p>
+                      </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+                      Q4: Is BMC Smart Reporting Cloud-based or on-premise?
+                       <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> It's flexible! You can choose based on your IT infrastructure, scalability needs, and other technical aspects.</p>
+                      </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+                      Q5: Why do I need a BMC Smart Reporting Service Provider?
+                          <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans: </strong> Setting it up for your specific needs requires expertise. Dot Global Technologies' BMC-certified team can help with implementation, customization, and ongoing support. We are the most trusted BMC Smart Reporting Support Service Provider in Bengaluru.</p>
+                     </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -20,16 +102,8 @@
 </template>
 
 <script>
-import commonFaq from '~~/mixins/bmcsmartreportingFAQ';
-import SingleFaq from './Faq/SingleFaq.vue';
 
 export default {
-  props: {
-    parent_id:String,
-  },
-  components:{
-    SingleFaq,
-  },
-  mixins:[commonFaq]
+
 };
 </script>
