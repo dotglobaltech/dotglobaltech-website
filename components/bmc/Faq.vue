230499dc3da411606853cb95a8eaa17c5e6b070d
dotglobaltech <dotglobaltech@gmail.com>
2024-04-12 12:03:33 +0530

    bmc main page done

--- components/bmc/Faq.vue
+++ components/bmc/Faq.vue
@@ -9,7 +9,99 @@
           <div class="col-xxl-9 col-xl-9 col-lg-9">
             <div class="faq__wrapper tp-accordion pt-65">
               <div class="accordion" id="faqaccordion">
-                  <single-faq v-for="(item,i) in faq_items" :key="i" :item="item" />
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                      Q1:  What is BMC?
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> BMC is a leading provider of enterprise software solutions for service management, workflow orchestration, automation, DevOps, AIOps, and security.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                      Q2: What is BMC Used For?
+
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong>  BMC solutions help businesses automate and improve IT operations, manage IT assets, optimize service delivery, and gain valuable insights into their IT environment.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                      Q3:   How Much Do BMC Solutions Cost?
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> The cost of BMC solutions varies depending on the specific products and services you need. Dot Global Technologies offers competitive pricing and can help you find a solution that fits your budget.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse3">
+                      Q4:    Is BMC Cloud-Based or On-Premise?
+
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> BMC offers various deployment options, including on-premise, cloud-based, and hybrid. Dot Global Technologies can help you choose the best-suited deployment model for your operational needs.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse3">
+                      Q5:   Why Work With a BMC Support Services Provider?
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Working with a <strong>BMC support services provider</strong> like Dot Global Technologies can help you ensure that your BMC solutions are implemented and maintained correctly. We have the expertise and experience to help you get the best ROI on your investment in BMC solutions.</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -20,16 +112,8 @@
 </template>
 
 <script>
-import commonFaq from '~~/mixins/BMCFAQ';
-import SingleFaq from './Faq/SingleFaq.vue';
 
 export default {
-  props: {
-    parent_id:String,
-  },
-  components:{
-    SingleFaq,
-  },
-  mixins:[commonFaq]
+
 };
 </script>
