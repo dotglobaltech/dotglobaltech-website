0ede75ca0b230bbb0243d2ffa6dcb98ca84a36b5
dotglobaltech <dotglobaltech@gmail.com>
2024-04-13 08:57:36 +0530

    salesforce main page changes done

--- components/salesforce/Faq.vue
+++ components/salesforce/Faq.vue
@@ -9,7 +9,94 @@
           <div class="col-xxl-9 col-xl-9 col-lg-9">
             <div class="faq__wrapper tp-accordion pt-65">
               <div class="accordion" id="faqaccordion">
-                  <single-faq v-for="(item,i) in faq_items" :key="i" :item="item" />
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                      Q1:  What is Salesforce?
+
+
+                         <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">     
+                      <p><strong>Ans:</strong> Salesforce is a San Francisco-based cloud software solutions company providing customer relationship management solutions and tools focused on sales, service management, analytics, marketing automation, and application development. As a<strong>Salesforce Partner</strong> , we customize and implement these solutions per your business-specific requirements.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                      Q2:   What is Salesforce used for?
+
+
+
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Salesforce is a globally renowned customer relationship management platform. Through our <strong>Salesforce Managed Services</strong> , we help you adopt this solution to unify your teams and deliver exceptional customer experiences.</p>
+                    
+                    </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                      Q3: What does the Salesforce company do?
+
+                      <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Salesforce develops cloud-based solutions to help businesses find prospects, win business, and enhance customer experiences. As reputed <strong>Salesforce vendors</strong>, we deploy and ensure your business maximizes these solutions.</p>
+                      </div>
+                  </div>
+                </div>
+
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+                      Q4: What are the benefits of Salesforce?
+
+                       <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans:</strong> Enjoy improved customer satisfaction, enhanced team collaboration, seamless reporting, excellent data accessibility, effective automation, and increased productivity with Salesforce.</p>
+                      </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+                      Q5:  Why do you need a Salesforce Service Provider?
+
+                          <span class="accordion-btn"></span>
+                    </button>
+                  </h2>
+                  <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading1"
+                    data-bs-parent="#parentAccordion">
+                    <div class="accordion-body">
+                      <p><strong>Ans: </strong> A <strong>Salesforce Third-Party Vendor</strong> deploys certified professionals to customize and implement solutions tailored to your business. Their expertise is crucial for effective customization, troubleshooting, and ongoing management, including providing <strong>Salesforce training </strong>for your IT teams.</p>
+                     </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -20,16 +107,8 @@
 </template>
 
 <script>
-import commonFaq from '~~/mixins/salesforceFAQ';
-import SingleFaq from './Faq/SingleFaq.vue';
 
 export default {
-  props: {
-    parent_id:String,
-  },
-  components:{
-    SingleFaq,
-  },
-  mixins:[commonFaq]
+
 };
 </script>
