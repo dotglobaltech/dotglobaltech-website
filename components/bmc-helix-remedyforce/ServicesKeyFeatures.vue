d376e9bf71b7912944cb2fc9cffb45681fde7539
Harshda-suryawanshi <harshdasuryawanshi93569@gmail.com>
2024-06-25 07:24:25 +0530

    changes done

--- components/bmc-helix-remedyforce/ServicesKeyFeatures.vue
+++ components/bmc-helix-remedyforce/ServicesKeyFeatures.vue
@@ -1,41 +1,71 @@
 <template>
   <section :class="`services__area ${style_2 ? 'pb-110' : el_style ? 'pt-110 pb-90' : 'pb-50'}`">
     <div class="container">
-      <div class="row justify-content-center pt-100">
-        <div class="col-xxl-12 col-xl-12 col-lg-10 col-md-10">
-          <div class="section__title-wrapper-7 mb-60 text-center">
-            <h3 class="section__title-7">
-              Unleash The Potential Of
-              <span class="section__title-7-highlight">
-                BMC Helix Remedyforce
-                <svg width="240" height="22" viewBox="0 0 240 22" fill="none">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M0.440833 21.1152C78.9497 6.16225 158.05 4.53412 235.949 13.8239C237.497 14.0088 239.796 12.4065 239.988 9.93474C240.181 7.4176 238.026 5.44088 236.474 5.2332C157.99 -5.31675 79.1936 0.359501 0.316568 19.7785C-0.184784 19.9023 -0.0511379 21.2092 0.440833 21.1152Z"
-                    fill="currentColor" />
-                </svg>
-              </span>
-              <h4>Streamlined Service Management</h4>
-            </h3>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div v-for="(item, i) in service_data" :key="i" class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+      <div class="row justify-content-center pt-80">
+        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
           <div :class="`services__item-9 ${style_2 ? 'services__item-style-2' : ''} mb-30 transition-3`">
             <div class="services__item-9-top d-flex align-items-start justify-content-between">
               <div class="services__icon-9">
                 <span>
-                  <span v-html="item.icon"></span>
-                  <img src="~/assets/img/services/9/services-icon-shape.png" alt="shape" />
+                  <span>
+                    <img src="~/assets/img/bmc-helix-remedyforce/Automate_Routine_Tasks-01.svg" alt="" />
+                  </span>
+                  <img src="~/assets/img/services/9/services-icon-shape.png" alt="" />
                 </span>
               </div>
             </div>
-            <div class="services__content-9" style="height:250px;">
+            <div class="services__content-9" style="height:300px;">
               <h3 class="services__title-9">
-                {{ item.title }}
+                Automate Routine Tasks
               </h3>
               <span class="services-project">
-                {{ item.description }}
+                Reduce manual work and free up your IT team to focus on more strategic initiatives. Through our incredible BMC Helix Remedyforce Managed Services, we implement workflow automation and help you streamline your routine tasks like never before.
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+          <div :class="`services__item-9 ${style_2 ? 'services__item-style-2' : ''} mb-30 transition-3`">
+            <div class="services__item-9-top d-flex align-items-start justify-content-between">
+              <div class="services__icon-9">
+                <span>
+                  <span>
+                    <img src="~/assets/img/bmc-helix-remedyforce/Improve_Efficiency-01.svg" alt="" />
+                  </span>
+                  <img src="~/assets/img/services/9/services-icon-shape.png" alt="" />
+                </span>
+              </div>
+            </div>
+            <div class="services__content-9" style="height:300px;">
+              <h3 class="services__title-9">
+               Improve Efficiency
+              </h3>
+              <span class="services-project">
+               Streamline workflows and processes to get things done faster and with fewer errors.
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+          <div :class="`services__item-9 ${style_2 ? 'services__item-style-2' : ''} mb-30 transition-3`">
+            <div class="services__item-9-top d-flex align-items-start justify-content-between">
+              <div class="services__icon-9">
+                <span>
+                  <span>
+                    <img src="~/assets/img/bmc-helix-remedyforce/Boost_ROI-01.svg" alt="" />
+                  </span>
+                  <img src="~/assets/img/services/9/services-icon-shape.png" alt="" />
+                </span>
+              </div>
+            </div>
+            <div class="services__content-9" style="height:300px;">
+              <h3 class="services__title-9">
+                Boost ROI
+              </h3>
+              <span class="services-project">
+                Maximize the return on your investment in IT service management with increased efficiency and productivity.
               </span>
             </div>
           </div>
@@ -47,49 +77,6 @@
 
 <script>
 export default {
-  props: {
-    style_2: {
-      type: Boolean,
-      default: false,
-    },
-    el_style: {
-      type: Boolean,
-      default: false,
-    }
-  },
-  data() {
-    return {
-      service_data: [
-        {
-          icon: `<svg width="47" height="42" viewBox="0 0 47 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.06035 29.4262L12.5801 37.0285C17.8613 42.3677 19.6652 42.2798 24.8812 37.0285L36.9867 24.79C41.203 20.5275 42.2679 17.6931 36.9867 12.3539L29.467 4.75152C23.838 -0.939241 21.3821 0.488942 17.1659 4.75152L5.06035 16.99C-0.133935 22.2633 -0.568603 23.7354 5.06035 29.4262Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path opacity="0.4" d="M38.5975 32.1286L37.1631 34.5236C35.1419 37.9292 36.7067 40.7197 40.6405 40.7197C44.5742 40.7197 46.139 37.9292 44.1178 34.5236L42.6834 32.1286C41.5533 30.239 39.7059 30.239 38.5975 32.1286Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path opacity="0.4" d="M1.21484 22.1312C13.2986 18.8134 26.0344 18.7036 38.1616 21.8456L39.2483 22.1312" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>`,
-          title: "Automate Routine Tasks",
-          description: " Reduce manual work and free up your IT team to focus on more strategic initiatives. Through our incredible BMC Helix Remedyforce Managed Services, we implement workflow automation and help you streamline your routine tasks like never before.",
-        },
-        {
-          icon: `<svg width="32" height="40" viewBox="0 0 32 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M31 10.5V29.5C31 37.1 29.125 39 21.625 39H10.375C2.875 39 1 37.1 1 29.5V10.5C1 2.9 2.875 1 10.375 1H21.625C29.125 1 31 2.9 31 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path opacity="0.4" d="M19.7637 7.65088H12.2637" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path opacity="0.4" d="M15.9941 33.4891C17.5992 33.4891 18.9004 32.1706 18.9004 30.5441C18.9004 28.9176 17.5992 27.5991 15.9941 27.5991C14.3891 27.5991 13.0879 28.9176 13.0879 30.5441C13.0879 32.1706 14.3891 33.4891 15.9941 33.4891Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>`,
-          title: "Improve Efficiency",
-          description: "Streamline workflows and processes to get things done faster and with fewer errors.",
-        },
-        {
-          icon: `<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path opacity="0.4" d="M33.2859 12.4C36.434 12.4 38.9859 9.84803 38.9859 6.7C38.9859 3.55198 36.434 1 33.2859 1C30.1379 1 27.5859 3.55198 27.5859 6.7C27.5859 9.84803 30.1379 12.4 33.2859 12.4Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path opacity="0.4" d="M10.5 21.9009H20" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path opacity="0.4" d="M10.5 29.5H27.6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M23.8 1H14.3C4.8 1 1 4.8 1 14.3V25.7C1 35.2 4.8 39 14.3 39H25.7C35.2 39 39 35.2 39 25.7V16.2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>`,
-          title: "Boost ROI",
-          description: "Maximize the return on your investment in IT service management with increased efficiency and productivity.",
-        },
-      ],
-    };
-  },
+
 };
 </script>
